ec0b23b1c300d526819cfc81c91b797558ab4137
benduran <stratodyne@gmail.com>
2021-08-03 20:03:52 -0700

    chore: have no idea why xterm.js isn't working
    only the first zsh linke
    is displaying. help

--- packages/client/src/components/XTerm/XTerm.tsx
+++ packages/client/src/components/XTerm/XTerm.tsx
@@ -3,7 +3,7 @@ import 'xterm/css/xterm.css';
 import { StdioMessageType } from '@experiterm/shared';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useCreateStyles } from 'simplestyle-js/esm/react';
-import { IEvent, Terminal } from 'xterm';
+import { Terminal } from 'xterm';
 
 import { useStdioSocketContext } from '../../context';
 
@@ -27,31 +27,39 @@ export const XTerm = ({ className }: XTermProps) => {
   const { messages, sendMessage } = useStdioSocketContext();
   const [terminalDiv, setTerminalDiv] = useState<HTMLDivElement | null>(null);
   const [xterm, setXterm] = useState<Terminal | null>(null);
+  const messagesLen = messages.length;
   const lastMessage = messages.length ? messages[messages.length - 1] : null;
+
+  const handleTerminalSendData = useCallback(
+    (message: string) => {
+      sendMessage({
+        message,
+        time: Date.now(),
+        type: StdioMessageType.STDIN,
+      });
+    },
+    [sendMessage],
+  );
 
   useEffect(() => {
     if (!xterm && terminalDiv) {
       const t = new Terminal({
+        cursorBlink: true,
+        logLevel: 'debug',
         rendererType: 'canvas',
       });
-      t.onData(msg => {
-        sendMessage({
-          message: msg,
-          time: Date.now(),
-          type: StdioMessageType.STDIN,
-        });
-      });
+      t.onData(handleTerminalSendData);
       t.open(terminalDiv);
       setXterm(t);
     }
-  }, [sendMessage, terminalDiv, xterm]);
+  }, [handleTerminalSendData, sendMessage, terminalDiv, xterm]);
 
   useEffect(() => {
     if (xterm && lastMessage) {
       console.info(`%cWriting message to xterm: ${lastMessage.message}`, 'font-weight: bold; color: blue;');
       xterm.write(lastMessage.message);
     }
-  }, [lastMessage, xterm]);
+  }, [lastMessage, messagesLen, xterm]);
 
   return <div className={`${classes.terminalDivWrapper} ${className || ''}`} ref={setTerminalDiv} />;
 };
